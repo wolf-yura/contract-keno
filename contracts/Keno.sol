4802934f5cb0e49bb642ced024f6afe2e6360a5a
wolf-yura <baba.takeshi95@gmail.com>
2021-08-11 15:26:01 -0400

    Updated
    Hopefully not to many compiling errors

--- contracts/Keno.sol
+++ contracts/Keno.sol
@@ -8,64 +8,48 @@ import "./interfaces/IUnifiedLiquidityPool.sol";
 
 contract Keno is Ownable, ReentrancyGuard {
     IUnifiedLiquidityPool public ULP;
-    IERC20 public GBTS;
+    IERC20 public token;
 
     uint256 constant gameId = 4;
-    uint256 constant gbtsBet = 5 * 10**17; // .5 GBTS to make a bet
+    uint256 constant tokenBet = 5 * 10**17; // .5 GBTS to make a bet
 
     /// @notice bettedGBTS keeps track of all GBTS brought in through ticket purchase
-    uint256 bettedGBTS;
+    uint256 totalBets;
 
     /// @notice wonGBTS keeps track of all GBTS won by players
-    uint256 wonGBTS;
+    uint256 totalWinnings;
 
     /// @notice bought ticket
     event TicketBought(address buyer);
 
     /// @notice ticket played
-    event TicketPlayed(
-        address player,
-        Ticket ticketPlayed,
-        uint256 winnings,
-        mapping(uint32 => bool) drawn
-    );
+    event TicketPlayed( address player, Ticket ticketPlayed, uint256 winnings, uint256[] drawn);
 
     /// @notice this is used to determine the winnings the player will receive
-    mapping(uint32 => uint256[]) winningTable; // divide by 100 to get proper multiplier
+    uint256[][] winningTable; // divide by 100 to get proper multiplier
 
     constructor(IUnifiedLiquidityPool _ULP, IERC20 _GBTS) {
         ULP = _ULP;
         GBTS = _GBTS;
-
-        winningTable[1] = [0, 326]; // 0, 3.26
-        winningTable[2] = [0, 0, 1141]; // 0, 0, 11.41
-        winningTable[3] = [0, 0, 200, 2600]; //  0, 0, 2.00, 26.00
-        winningTable[4] = [0, 0, 0, 800, 7200]; // 0, 0, 0, 8.00, 72.00
-        winningTable[5] = [0, 0, 0, 100, 1600, 25490]; // 0, 0, 0, 1.00, 16.00, 254.90
-        winningTable[6] = [0, 0, 0, 200, 200, 4500, 65000]; // 0, 0, 0, 2.00, 2.00, 45.00, 650.00
-        winningTable[7] = [100, 0, 0, 0, 200, 1100, 15000, 425000]; //1.00, 0, 0, 0, 2.00, 11.00, 150.00, 4250.00
-        winningTable[8] = [100, 0, 0, 0, 100, 500, 3500, 40000, 2140000]; // 1.00, 0, 0, 0, 1.00, 5.00, 35.00, 400.00, 21400.00
-        winningTable[9] = [100, 0, 0, 0, 0, 300, 1600, 18000, 200000, 5000000]; // 1.00, 0, 0, 0, 0, 3.00, 16.00, 180.00, 2000.00, 50000.00
-        winningTable[10] = [
-            100,
-            0,
-            0,
-            0,
-            0,
-            300,
-            700,
-            2000,
-            50000,
-            1000000,
-            20000000
-        ]; //1.00, 0, 0, 0, 0, 3.00, 7.00, 20.00, 500.00, 10000.00, 200000.00
+        winningTable.push()
+        winningTable.push([0, 326]); // 0, 3.26
+        winningTable.push([0, 0, 1141]); // 0, 0, 11.41
+        winningTable.push([0, 0, 200, 2600]); //  0, 0, 2.00, 26.00
+        winningTable.push([0, 0, 0, 800, 7200]); // 0, 0, 0, 8.00, 72.00
+        winningTable.push([0, 0, 0, 100, 1600, 25490]); // 0, 0, 0, 1.00, 16.00, 254.90
+        winningTable.push([0, 0, 0, 200, 200, 4500, 65000]); // 0, 0, 0, 2.00, 2.00, 45.00, 650.00
+        winningTable.push([100, 0, 0, 0, 200, 1100, 15000, 425000]); //1.00, 0, 0, 0, 2.00, 11.00, 150.00, 4250.00
+        winningTable.push([100, 0, 0, 0, 100, 500, 3500, 40000, 2140000]); // 1.00, 0, 0, 0, 1.00, 5.00, 35.00, 400.00, 21400.00
+        winningTable.push([100, 0, 0, 0, 0, 300, 1600, 18000, 200000, 5000000]); // 1.00, 0, 0, 0, 0, 3.00, 16.00, 180.00, 2000.00, 50000.00
+        winningTable.push([100, 0, 0, 0, 0, 300, 700, 2000, 50000, 1000000, 20000000]); //1.00, 0, 0, 0, 0, 3.00, 7.00, 20.00, 500.00, 10000.00, 200000.00
     }
 
     struct Ticket {
-        uint32 choose;
-        uint32[] chosen;
-        uint256 gameRandomNumber;
+        uint256 choose;
+        uint256[] chosen;
+        bytes32 batchID;
         bool played;
+        uint256[] drawn;
     }
 
     mapping(address => uint256) currentTicket;
@@ -75,103 +59,82 @@ contract Keno is Ownable, ReentrancyGuard {
      * @dev External function for buying ticket.
      * @param _chosen Array of numbers chosen
      */
-    function buyTicket(uint32[] _chosen) external nonReentrant {
-        uint32 length = _chosen.length;
+    function buyTicket(uint256[] _chosen) external nonReentrant {
+        uint256 length = _chosen.length;
         require(length <= 10, "To Many chosen");
         require(length > 0, "No number chosen");
         require(
-            GBTS.transferFrom(msg.sender, address(ULP), gbtsBet) == true,
+            token.transferFrom(msg.sender, address(ULP), tokenBet) == true,
             "Bet not transferred"
         );
-        bettedGBTS += gbtsBet;
-        Ticket memory ticket = Ticket(length, _chosen, ULP.getRandomNumber());
+        totalBets += tokenBet;
+        Ticket memory ticket;
+        ticket.choose = length;
+        ticket.chosen = _chosen
+        ticket.batchID = ULP.getRandomNumber();
         playerTickets[msg.sender].push(ticket);
 
         emit TicketBought(msg.sender);
     }
 
-    /**
-     * @dev Internal function to pay any winnings.
-     * @param _matches Total number of matches made with the draw
-     * @param _draw Mapping of draw
-     */
-    function payWinnings(uint32 _matches, mapping(uint32 => bool) _draw)
-        internal
-    {
-        Ticket storage ticket = playerTickets[msg.sender][
-            currentTicket[msg.sender]
-        ];
-        uint256 multiplier = winningTable[ticket.choose][_matches];
-        currentTicket[msg.sender] += 1;
-        uint256 amountToSend = (multiplier * gbtsBet) / 100;
-        ticket.played = true;
-
-        if (amountToSend > 0) {
-            wonGBTS += amountToSend;
-            ULP.sendPrize(msg.sender, amountToSend);
-        }
-        emit TicketPlayed(msg.sender, ticket, amountToSend, _draw);
-    }
 
     /**
      * @dev External function to play.
      */
     function play() external nonReentrant {
         uint256 startingTicketNumber = currentTicket[msg.sender];
-        Ticket[] ticketsBought = playerTickets[msg.sender];
         require(
-            startingTicketNumber <= ticketsBought.length,
+            startingTicketNumber < playerTickets[msg.sender].length,
             "No ticket to Play"
         );
+        
+        Ticket storage ticket = playerTickets[msg.sender][startingTicketNumber];
+
+        currentTicket[msg.sender] = startingTicketNumber + 1;
+
+        require(!ticket.played, "Ticket already Played");
 
         uint256 currentRandom = ULP.getNewRandomNumber(
-            ticketsBought[startingTicketNumber].gameRandomNumber
-        );
-        mapping(uint32 => bool) drawNumbers = draw(
-            currentRandom,
-            startingTicketNumber
+            ticket.batchID
         );
 
-        uint32 matches;
-        uint32[] ticket = ticketBought[startingTicketNumber].chosen;
-        for (uint32 i = 0; i < ticketBought[startingTicketNumber].choose; i++) {
-            if (drawNumbers[ticket[i]]) {
-                matches += 1;
-            }
-        }
-        payWinnings(matches, drawNumbers);
-    }
-
-    /**
-     * @dev Internal function to draw.
-     * @param random random number
-     * @param ticket Ticket number
-     */
-    function draw(uint256 random, uint256 ticket)
-        internal
-        view
-        returns (mapping(uint32 => bool))
-    {
-        mapping(uint32 => bool) draw;
-        uint32 size = 0;
-        uint256 count = ticket;
+        //Draws numbers using the Random Number returned
+        mapping(uint256 => bool) draw;
+        uint256 size = 0;
+        uint256 count = startingTicketNumber;
         while (size < 15) {
-            uint32 gameNumber = uint32(
-                keccak256(
-                    abi.encode(
-                        newRandomNumber,
-                        address(msg.sender),
-                        gameId,
-                        count
-                    )
-                )
-            ) % 50;
+            uint256 gameNumber = uint256(keccak256(abi.encode(currentRandom,address(msg.sender),gameId,count))) % 50;
             count += 1;
             if (!draw[gameNumber]) {
                 draw[gameNumber] = true;
+                ticket.drawn.push(gameNumber);
                 size += 1;
             }
         }
-        return draw;
+
+        uint256 matches=0;
+        //match the players choice with the draw
+        for (uint32 i = 0; i < ticket.choose; i++) {
+            if (draw[ticket[i]]) {
+                matches += 1;
+            }
+        }
+
+        uint256[] memory _choice = winningTable[ticket.choose]; //The multiplier array of the choose size
+        uint256 multiplier = _choice[matches]; //Multiplier 
+        uint256 amountToSend = 0;
+        if(multiplier > 0){
+
+            amountToSend = (multiplier * tokenBet) / 100;
+        }
+
+        ticket.played = true;
+
+        if (amountToSend > 0) {
+            totalWinnings += amountToSend;
+            ULP.sendPrize(msg.sender, amountToSend);
+        }
+        emit TicketPlayed(msg.sender, ticket, amountToSend, ticket.drawn);
     }
+
 }
