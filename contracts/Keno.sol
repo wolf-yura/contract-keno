d52293e03f7196e2a77ba06c760178cc8fffaea7
wolf-yura <baba.takeshi95@gmail.com>
2021-08-17 09:33:15 +0800

    Used SafeERC20

--- contracts/Keno.sol
+++ contracts/Keno.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/IUnifiedLiquidityPool.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Keno is ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     /// @notice Event emitted when user bought the tickets.
     event TicketsBought(address buyer, uint256 ticketNumber);
 
@@ -85,7 +88,7 @@ contract Keno is ReentrancyGuard {
             "Keno: Every ticket should have 1 to 11 numbers."
         );
 
-        GBTS.transferFrom(msg.sender, address(ULP), betAmount);
+        GBTS.safeTransferFrom(msg.sender, address(ULP), betAmount);
         totalBettedAmount += betAmount;
         uint256 _ticketNumber = playerTickets[msg.sender].length;
         Ticket memory ticket;
