29a5a708ecb67f384b8bb28ed892b1a1609eec20
wolf-yura <baba.takeshi95@gmail.com>
2021-08-17 09:33:52 +0800

    Update interface

--- contracts/Keno.sol
+++ contracts/Keno.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./interfaces/IUnifiedLiquidityPool.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/IUnifiedLiquidityPool.sol";
 
 contract Keno is ReentrancyGuard {
     using SafeERC20 for IERC20;
