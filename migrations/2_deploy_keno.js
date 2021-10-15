60e26f578816568f3b990916156027889b15310a
wolf-yura <baba.takeshi95@gmail.com>
2021-10-16 02:21:20 +0800

    Update migration

--- migrations/2_deploy_keno.js
+++ migrations/2_deploy_keno.js
@@ -4,9 +4,9 @@ module.exports = async function (deployer) {
 
     await deployer.deploy(
         Keno,
-        "0x5224C8942e318aAb995Ff50F07cf90ddd34EaDDf", // Deployed ULP Address
-        "0x6db26bDE37C4201eCedB71d669e89e935Fea4ccF", // Deployed GBTS Address
-        4, // Game Id
+        "0xB1310c700699eC24D67beAc7164cea0792069080", // Deployed ULP Address
+        "0xC238001552195f0E159798CdA94DC22a71993bC9", // Deployed GBTS Address
+        "0xfe18A7673bAa26870B4f8df6a9cCE811E1f88e52"  // Deployed RNG Address
     );
 
     return;
